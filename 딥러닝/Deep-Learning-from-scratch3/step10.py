bc82850a53bebc479e7f4d0f54249a52b9387547
박성우 <psw@pswMacBookPro.local>
2024-02-13 17:54:55 +0900

    depp learning 3

---
+++ 딥러닝/Deep-Learning-from-scratch3/step10.py
@@ -0,0 +1,73 @@
+import unittest
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{format(type(data))}은(는) 지원하지 않습니다.')
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))  # 함수 계산 후, 형변환이 일어나는걸 방지
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x**2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy  # gy는 출력쪽에서 전해지는 미분값을 전달하는 역할
+        return gx
+
+
+def square(x):
+    f = Square()
+    return f(x)
+
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+
+"""
+터미널에서 step10.py 스크립트 실행후 확인
+실행 명령어 => python3 -m unittest step10.py
+"""
